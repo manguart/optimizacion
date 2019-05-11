67afbb496d6d9483d8c2e5977d07619bc3188b33
Antonio Manguart Páez <antonio@manguart.com>
2019-05-11 12:54:25 -0500

    Remove code

--- newton.py
+++ newton.py
@@ -13,24 +13,3 @@ def newton(f, df, x0, epsilon):
 
 newton(f, df, 1, 1e-8)
 
-
-
-%spark.pyspark
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
-from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
-
-df_assembler = VectorAssembler(inputCols=['longitud', 'latitud'], outputCol='features')
-profeco = df_assembler.transform(df)
-model_profeco = titanic.select(['features','precio'])
-train_df,test_df = model_profeco.randomSplit([0.8,0.2])
-
-rf=RandomForestRegressor(labelCol='precio')
-paramGrid = ParamGridBuilder()\
-    .addGrid(rf.maxDepth, [5, 10, 20]) \
-    .addGrid(rf.numTrees, [20, 50, 75]) \
-    .build()
-
-tvs = TrainValidationSplit(estimator=rf, estimatorParamMaps=paramGrid,evaluator=RegressionEvaluator(labelCol='precio'),trainRatio=0.8)
-model2 = tvs.fit(train_df)
-model2_predictions= model2.transform(test_df)
