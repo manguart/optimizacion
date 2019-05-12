6c218d4dc9c7698cf99830bd5e888cbfe02df2dd
Antonio Manguart Páez <antonio@manguart.com>
2019-05-11 19:55:42 -0500

    Modificación

--- markowitz.py
+++ markowitz.py
@@ -3,20 +3,20 @@ This script optimize portfolio returns using Markowitz curve
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import cvxopt
-from cvxopt import blas, solvers
+from cvxopt import blas, solvers, matrix
 import pandas as pd
 import matplotlib
+import random
 matplotlib.use('Agg')
 from matplotlib.backends.backend_pdf import PdfPages
 
 return_data = pd.read_csv('monthly_return.csv')
 return_data = return_data[[i for i in return_data.keys() if i not in ('date')]]
 return_data = return_data.T.values
-n_portfolios = 50000
+n_portfolios = 1000000
 mean_returns = np.mean(return_data, axis=1)
 sigma_returns = np.cov(return_data)
-number_of_optimal_values = 200
+number_of_optimal_values = 5
 
 
 def generate_portfolio():
@@ -34,37 +34,32 @@ def generate_portfolio():
     risk = np.sqrt(weights_matrix * covariance_matrix * weights_matrix.T)
     return expected_return, risk
 
+def iif(cond, iftrue=1.0, iffalse=0.0):
+    return iftrue if cond else iffalse
 
-def optimization(N):
-    """
-    Find optimal solutions using cvxopt package
-    """
-    total_stocks = len(return_data)
-    mus = [10 ** (5.0 * t / N - 1.0) for t in range(N)]
-    # Covariance matrix
-    Sigma = cvxopt.matrix(sigma_returns)
-    # Mean matrix
-    mean_return_cvx = cvxopt.matrix(mean_returns)
-    # Identity matrix
-    G = -cvxopt.matrix(np.eye(total_stocks))
-    # Vector of zeros
-    h = cvxopt.matrix(0.0, (total_stocks, 1))
-    # Vector of ones
-    A = cvxopt.matrix(1.0, (1, total_stocks))
-    # Just 1x1 matrix
-    b = cvxopt.matrix(1.0)
 
-    portfolios = [solvers.qp(mu * Sigma, -mean_return_cvx,
-                             # Constraints
-                             G, h, A, b)['x'] for mu in mus]
-    # Multiply weights per expected return
-    expected_returns = [blas.dot(mean_return_cvx, p) for p in portfolios]
-    # Ger variance
-    portfolio_risk = [np.sqrt(blas.dot(p, Sigma * p)) for p in portfolios]
-    m1 = np.polyfit(expected_returns, portfolio_risk, 2)
-    x1 = np.sqrt(m1[2] / m1[0])
-    wt = np.asarray(solvers.qp(cvxopt.matrix(x1 * Sigma), -mean_return_cvx, G, h, A, b)['x'])
-    return wt, expected_returns, portfolio_risk
+def markowitz(mu, sigma, mu_p):
+    d = len(sigma)
+    P = matrix(sigma)
+    q = matrix([0.0 for i in range(d)])
+
+    G = matrix([
+        [(-1.0) ** (1 + j % 2) * iif(i == j / 2) for i in range(d)]
+        for j in range(2 * d)
+    ]).trans()
+    h = matrix([iif(j % 2) for j in range(2 * d)])
+
+    A = matrix([
+        [1.0 for i in range(d)],
+        list(mu)
+    ]).trans()
+    b = matrix([1.0, float(mu_p)])
+
+    sol = solvers.qp(P, q, G, h, A, b)
+
+    w = list(sol['x'])
+    return w
+
 
 def main():
     """
@@ -75,14 +70,24 @@ def main():
     random_risk = [j[0] for j in random_risk]
     random_return = [k[0] for k in random_return]
 
-    # Optimal portfolio
-    wt, optimal_returns, optimal_risks = optimization(number_of_optimal_values)
+    returns_random_low = random.sample(random_return,k=20000)
+    optimals = []
+    [optimals.append(markowitz(mean_returns, sigma_returns, i)) for i in returns_random_low]
+
+    optimal_means = []
+    for i in optimals:
+        optimal_means.append(np.dot(i, mean_returns))
+
+    optimal_variance = []
+    for i in optimals:
+        variance_i = np.asmatrix(i)
+        optimal_variance.append(np.sqrt(float(variance_i * sigma_returns * variance_i.T)))
 
     # Plot results
     pdf = PdfPages('markowitz_curve.pdf')
     plt.figure()
     plt.plot(random_risk, random_return, 'o', markersize=5, alpha=0.5)
-    plt.plot(optimal_risks, optimal_returns, 'o', alpha=0.7, color='red')
+    plt.plot(optimal_variance, optimal_means, 'o', alpha=0.7, color='red')
     plt.xlabel('Risk')
     plt.grid()
     plt.ylabel('Return')
@@ -93,3 +98,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
