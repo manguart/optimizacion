51d3e47c21c019fcc7d8f30422c21a8802e79a1b
edroga <ilan2884@hotmail.com>
2019-05-28 10:36:42 -0500

    Update stock_data.R

--- stock_data.R
+++ stock_data.R
@@ -1,45 +1,69 @@
-# This script download some stocks (tickets) and create som .csv and time series for exploratory purposes
+##############################################################################
+# Esta función sirve para bajar los precios diarios y los retornos mensuales
+# de un conjunto de acciones dadas. Recibe como input un conjunto de acciones
+# fechas inicial y fecha final, el output de la función es una lista que 
+# contiene un data frame con los precios diarios y otro data frame con los 
+# rendimientos mensuales
+
+##############################################################################
+# cargamos las librerias que seran usadas
 library(quantmod)
-library(corrplot)
-# The user can set this dates
-start_date = as.Date("2002-01-01")
-end_date= as.Date("2019-03-31")
-# Also, user can set stocks
-tickets <- c("AAPL", "AMZN", "MSFT", "KO", "PEP", "JPM", "BAC", "NKE", "ORCL", "IBM", "PG", "WMT")
-portafolio_daily_prices <- NULL
-portafolio_monthly_return <- NULL
-for (Ticker in tickets){
-   # Obtener precios
-   prices <- getSymbols.yahoo(Ticker,
-                              from=start_date, to = end_date,
-                              periodicity = "daily", auto.assign=FALSE)[,4]
-   portafolio_daily_prices <- cbind(portafolio_daily_prices, prices)
-   # Generar grafico
-   png(paste(Ticker, ".png", sep = ""))
-   chartSeries(prices, name = Ticker)
-   dev.off()
-   # Rendimiento mensual
-   portafolio_monthly_return <- cbind(portafolio_monthly_return, monthlyReturn(prices))
+library(purrr)
+library(readr)
+library(dplyr)
+library(tibble)
+##############################################################################
+# Definimos la función
+
+obtener_rendimientos<-function(Activos, Fecha_inicial, Fecha_final){
+  
+  # bajamos los activos
+  portafolio_prices<-Activos %>% 
+    map(~getSymbols.yahoo(.,
+                          from=Fecha_inicial, 
+                          to = Fecha_final,
+                          periodicity = "daily", 
+                          auto.assign=FALSE)[,4])
+  
+  # Obtenemos los precios diarios a partir de portafolio_prices
+  precios_diarios<-portafolio_prices %>% 
+    map(~as.data.frame(.) %>%
+          rownames_to_column(var = "date")) %>% 
+    reduce(left_join, by = "date") %>% 
+    set_names(c("date", Activos)) %>% 
+    mutate(date = date %>% as.Date)
+  
+  # Obtenemos los rendimientos mensuales a partir de portafolio_prices
+  rendimientos_mensuales<-portafolio_prices %>% 
+    map(~monthlyReturn(.) %>% 
+          as.data.frame %>% 
+          rownames_to_column(var = "date")) %>% 
+    reduce(left_join, by = "date") %>% 
+    set_names(c("date", Activos)) %>% 
+    mutate(date = date %>% as.Date)
+  
+  precios_y_rendimientos<-list(PreciosDiarios = precios_diarios, 
+                               RendimientosMensuales = rendimientos_mensuales)
+  
+  return(precios_y_rendimientos)
 }
 
-# Clean dataset, add it to a .csv
-names_portafolio_prices <- names(portafolio_daily_prices)
+##############################################################################
+################################### Ejemplo ##################################
+##############################################################################
 
-replace_close <- function(str){
-   return(gsub(pattern = ".Close", x = str, replacement = ""))
-}
+# Seleccionamos algunas acciones
+activos<-c("AAPL", "AMZN", "MSFT", "KO", "PEP", "JPM", 
+           "BAC", "NKE", "ORCL", "IBM", "PG", "WMT")
 
-names_columns <- sapply(names_portafolio_prices, replace_close)
+# Obtenemos los rendimientos
+preciosd_y_retornos_mensuales<-activos %>% 
+  obtener_rendimientos(Fecha_inicial = as.Date("2002-01-01"),
+                       Fecha_final = as.Date("2019-03-31"))
 
-names(portafolio_monthly_return) <- names_columns
-names(portafolio_daily_prices) <- names_columns
+portafolio_daily_prices<-preciosd_y_retornos_mensuales$PreciosDiarios
+portafolio_monthly_return<-preciosd_y_retornos_mensuales$RendimientosMensuales
+##############################################################################
 
-# Write to .csv
-portafolio_daily_prices <- data.frame(portafolio_daily_prices)
-portafolio_monthly_return <- data.frame(portafolio_monthly_return)
-
-portafolio_monthly_return$date <- as.Date(index(portafolio_monthly_return))
-portafolio_daily_prices$date <- as.Date(index(portafolio_daily_prices))
-
-write.csv(x = portafolio_monthly_return, file = "monthly_return.csv", row.names=FALSE)
-write.csv(x = portafolio_daily_prices, file = "daily_price.csv", row.names=FALSE)
+write_csv(portafolio_daily_prices, "daily_price.csv")
+write_csv(portafolio_monthly_return, "monthly_return.csv")
