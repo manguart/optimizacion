116f0aab36ab9008efae6b49dae7360d33081996
edroga <ilan2884@hotmail.com>
2019-05-28 10:30:45 -0500

    Update markowitz.py

--- markowitz.py
+++ markowitz.py
@@ -1,93 +1,65 @@
-"""
-This script optimize portfolio returns using Markowitz curve
-"""
+##############################################################
+######### se importan las librerias que seran usadas #########
 import numpy as np
-import matplotlib.pyplot as plt
-import cvxopt
 import pandas as pd
-import matplotlib
-import random
-matplotlib.use('Agg')
-from matplotlib.backends.backend_pdf import PdfPages
+from cvxopt import blas, solvers, matrix
 
+def markowitz(r_i, sigma, rho, Ventas_en_corto = False):
+    ##########################################################
+    # Se defienen las matrices principales con la equivalencia
+    # entre el problema general de optimización y el problema 
+    # de optimización de markowitz
+    ##########################################################
+    d = len(sigma)
+    P = matrix(sigma)
+    q = matrix(np.zeros(d, float))
+    A = matrix([
+        np.ones(d, float).tolist(), r_i.tolist()
+    ]).trans()
+    b = matrix([1.0, float(rho)])
+    ##########################################################
+    # Se define la restricción para cuando las ventas en corto
+    # no están permitidas
+    ##########################################################
+    diagonal = np.zeros((d, d), float)
+    np.fill_diagonal(diagonal, -1.0)
+    
+    G = matrix(diagonal)
+    h = matrix(np.zeros(d, float))
+    ##########################################################
+    # Se revisa la condición de Ventas en corto para asignar 
+    # valores a G y h
+    ##########################################################
+    if Ventas_en_corto == True:
+        G=None
+        h=None
+    else:
+        G=G
+        h=h
+    ##########################################################
+    # Una vez que están definidos todos los elementos se 
+    # resuelve el problema usando solvers.qp
+    ##########################################################
+    sol = solvers.qp(P=P, q=q, G=G, h=h, A=A, b=b)
+    w = list(sol['x'])
+    ##########################################################
+    return w
+
+###############################################################################
+################################### Ejemplo ###################################
+###############################################################################
+
+# Cargamos nuestros datos
 return_data = pd.read_csv('monthly_return.csv')
 return_data = return_data[[i for i in return_data.keys() if i not in ('date')]]
 return_data = return_data.T.values
-n_portfolios = 100000
+
+# Obtenemos los arreglos que seran usados como inputs
 mean_returns = np.mean(return_data, axis=1)
 sigma_returns = np.cov(return_data)
 
+# Cálculamos los pesos para un rendimiento dado de .009 permitiendo ventas en corto
+markowitz(mean_returns, sigma_returns, .009, Ventas_en_corto = True)
 
-def generate_portfolio():
-    """
-    For a given matrix with returns, generate random portfolio
-    """
-    w = np.random.random(len(return_data))
-    w /= np.sum(w)
-
-    x = np.asmatrix(mean_returns)
-    w_matrix = np.asmatrix(w)
-    covariance_matrix = np.asmatrix(sigma_returns)
-
-    expected_return = w_matrix * x.T
-    risk = np.sqrt(w_matrix * covariance_matrix * w_matrix.T)
-    return risk, expected_return
-
-
-def markowitz(m, covariance_matrix, expected_return):
-    """
-    Maximize W^T S W
-    st:
-    sum(W) = 1
-    np.dot(w,r) = m
-    Where:
-    m = expected return for a given combination
-    S = covariance matrix
-    W = weights
-    """
-    condition_check = lambda cond: 1.0 if cond else 0.0
-
-    d = len(covariance_matrix)
-    p = cvxopt.matrix(covariance_matrix)
-    q = cvxopt.matrix([0.0 for i in range(d)])
-
-    g = cvxopt.matrix([[(-1.0) ** (1 + j % 2) * condition_check(i == j / 2)
-                        for i in range(d)] for j in range(2 * d)]).trans()
-
-    h = cvxopt.matrix([condition_check(k % 2) for k in range(2 * d)])
-
-    a = cvxopt.matrix([[1.0 for i in range(d)], list(m)]).trans()
-    b = cvxopt.matrix([1.0, float(expected_return)])
-    return list(cvxopt.solvers.qp(p, q, g, h, a, b)['x'])
-
-
-def main():
-    """
-    General calls
-    """
-    # Get random portfolio
-    random_risk, random_return = np.column_stack([generate_portfolio() for i in range(n_portfolios)])
-    random_risk = [j[0] for j in random_risk]
-    random_return = [k[0] for k in random_return]
-
-    returns_random_low = random.sample(random_return, k=2000)
-    optimals = [markowitz(mean_returns, sigma_returns, i) for i in returns_random_low]
-    optimal_means = [np.dot(i, mean_returns) for i in optimals]
-
-    optimal_variance = [np.sqrt(float(np.asmatrix(i) * sigma_returns * np.asmatrix(i).T)) for i in optimals]
-
-    # Plot results
-    pdf = PdfPages('markowitz_curve.pdf')
-    plt.figure()
-    plt.plot(random_risk, random_return, 'o', markersize=5, alpha=0.5)
-    plt.plot(optimal_variance, optimal_means, 'o', alpha=0.7, color='red')
-    plt.xlabel('Risk')
-    plt.grid()
-    plt.ylabel('Return')
-    plt.title('Markowitz curve')
-    pdf.savefig()
-    plt.close()
-    pdf.close()
-
-if __name__ == "__main__":
-    main()
+# Cálculamos los pesos para un rendimiento dado de .009 solo con pesos positivos
+markowitz(mean_returns, sigma_returns, .009, Ventas_en_corto = True)
